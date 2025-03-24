96d882ebc9703cefdbb895655de5fabdfcc46608
Siwani <kaushik10siwani@gmail.com>
2025-03-25 00:26:01 +0530

    day 3 code

---
+++ .github/01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 1445   //const ca not be changed
+let accountMail ="Kausik@google.com"
+var accountPasword ="12345"
+accountCity = "jaipur"
+//accountId =122    
+console.log(accountId);
+accountMail ="abacd"
+accountPasword="244"
+accountCity="mumbai"
+let accountState;
+console.table([accountId,accountMail,accountPasword,accountCity,accountState]);
+/*
+prefer not to use vae
+beacuse of issue in block scope & functional scope
+*/
+ 
---
+++ .github/01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+//alert( 3 + 3)//we are usimg node.js not browser
+
+let name = "hitesh"
+let age =12
+let isLoggedIn = true
+console.table([name,age,isLoggedIn])
+
+// number => 2 to power 53
+//bigint
+//string =""
+// boolean => true/false
+// null = standalone value
+// undefined => no value is assigned now
+//symbol => unique
+
+//object
+
+console.log(typeof"hitesh");
+console.log(typeof null);
+console.log(typeof undefined);
