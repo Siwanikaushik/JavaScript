f81a5225f9614d797f936993293b05cc3dc592c4
Siwani <kaushik10siwani@gmail.com>
2025-03-25 23:53:44 +0530

    day_4_code

---
+++ .github/01_basics/03_datatype_conversions.js
@@ -0,0 +1,29 @@
+let score = "Hitesh"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof (valueInNumber));
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// "true"=> 1
+// null => 0
+// undefined => NaN
+ 
+let isLoggedIn = "happy"
+let BolleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(isLoggedIn);
+console.log(BolleanIsLoggedIn);
+console.log(typeof BolleanIsLoggedIn);
+
+// 1=> ture , 0=.false
+//"string"=> true
+// "" => false
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
---
+++ .github/01_basics/04_operations.js
@@ -0,0 +1,31 @@
+let value =3
+let negValue= -value
+console.log(negValue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/2)
+console.log(2%2)
+
+let str="hello"
+let str2="siwani"
+let str3=str+str2;
+console.log(str3);
+console.log(1+"2")
+console.log("1"+2)
+console.log("1"+2+2)
+console.log(2+3+"1")
+console.log(true)
+console.log(+true) //=> 1
+console.log(+"")
+
+
+let num1,num2,num3
+num1=num2=num3= 2+2;
+console.log(num1,num2,num3)
+
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter)
