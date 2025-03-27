d073acbd92be1b5991580e1d3068f875c830114e
Siwani <kaushik10siwani@gmail.com>
2025-03-27 15:06:26 +0530

    day_5 code

--- .github/01_basics/datatypes_summary.js
+++ .github/01_basics/datatypes_summary.js
@@ -18,5 +18,22 @@ Array , objects , functions ,
 const value=Symbol("123")
 const id=Symbol("123")
 console.log(value == id)
-const bigNumber = 43658708965432987564534n
+const bigNumber = 43658n
 console.log(typeof bigNumber)
+const outside = null //object
+
+//arrays
+const hero=["shaktiman","naagraj","doga"];
+//objects
+let myobj ={
+    name :"lakshay",
+    age : 33,
+}
+// Function
+const myfunction= function(){
+    console.log("Hello World.")
+}
+ console.log(typeof outside)
+ console.log(typeof hero)
+ console.log(typeof myobj)
+ console.log(typeof myfunction)
